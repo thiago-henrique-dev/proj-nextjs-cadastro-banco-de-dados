0394fc3efb7b98c8a7698839fbb01ce986d63eea
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-06 14:17:07 -0300

    Componente Layout

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,7 +2,7 @@ export default function Cabecalho(props) {
     // props é somente leitura
     return (
         <header>
-            <h1>{props.titulo}</h1>
+            <h1>{props.titulo }</h1>
         </header>
     )
 }
--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,10 +1,15 @@
 import styles from '../styles/Estiloso.module.css'
 import Link from 'next/link'
+import Layout from '../components/Layout'
 export default function Estiloso(){
     return (
-        <div className={styles.roxo}>
-            <Link href="/">Voltar</Link>
-            <h1>Estilo usando CSS Módulos</h1>
+
+        <Layout titulo="Exemplo de CSS Modularizado">
+         <div className={styles.roxo}>
+   
+         <h1>Estilo usando CSS Módulos</h1>
         </div>
+        </Layout>
     )
+      
 }
