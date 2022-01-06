886c3d637633baccd07feeb5f6c1afb3c408e49b
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-06 13:33:47 -0300

    Nevagação entre Componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,7 +1,9 @@
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link'
 export default function Estiloso(){
     return (
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
     )
